7ecc24003e4c75105eca0262e865781336f0ea60
Jaden C Van <jcvan80@gmail.com>
2019-07-26 04:12:56 -0400

    routes not yet completed

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-//NPM packages
-
 var express = require("express");
-var bodyParser = require("body-parser");
-
-
 var app = express();
+var path = require("path");
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-app.listen(port, function(){
-    console.log( "App listening on PORT: " + port);
+app.listen(PORT, function(){
+    console.log("App listening on: "+PORT);
 });
